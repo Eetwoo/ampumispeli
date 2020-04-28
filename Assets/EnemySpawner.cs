d8b5b4d0d1276aa4422e51e0d8f3b1121de70cfb
Eetwoo <Eetwoo@users.noreply.github.com>
2020-04-28 19:34:07 +0300

    asd

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -2,23 +2,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject enemyPrefab1;
     float delay = 5.0f;
     float canspawn = 5;
     public float randX, randY, randZ;
+    int WaveNumber = 1;
+    int NumberOfEnemies = 3;
+    int enemiesByWave;
+    [SerializeField]int moreEnemiesByWave;
     // Start is called before the first frame update
     void Start()
     {
         
-    }
+    }                                                                   
 
     // Update is called once per frame
     void Update()
     {
-        SpawnEnemy();
+        if(NumberOfEnemies >= 0)
+        {
+            SpawnEnemy();
+        }
+        else
+        {
+            
+            Invoke("ResetWave", 5f);
+        }
+
+    }
+
+    private void ResetWave()
+    {
+        enemiesByWave += moreEnemiesByWave;
+        NumberOfEnemies = enemiesByWave;
+        WaveNumber += 1;
     }
 
     private void SpawnEnemy()
@@ -29,8 +51,9 @@ public class EnemySpawner : MonoBehaviour
             randY = UnityEngine.Random.Range(1.0f, 2.0f);
             randZ = UnityEngine.Random.Range(-5.0f, 8.0f);
 
-            enemyPrefab = Instantiate(enemyPrefab, new Vector3(randX, randY, randZ), transform.rotation) as GameObject;
+            enemyPrefab1 = Instantiate(enemyPrefab, new Vector3(randX, randY, randZ), transform.rotation) as GameObject;
             canspawn += delay;
+            NumberOfEnemies = NumberOfEnemies - 1;
         }
     }
 }
