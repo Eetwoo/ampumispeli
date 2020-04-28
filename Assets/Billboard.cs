d96eb53ecbb9e5009fd77f866b26e3daaef0ad42
Niko Koivu <niko.koivu@gmail.com>
2020-04-28 21:01:12 +0300

    billboard fix

--- Assets/Billboard.cs
+++ Assets/Billboard.cs
@@ -9,6 +9,6 @@ public class Billboard : MonoBehaviour
 
     void LateUpdate()
     {
-        transform.LookAt(transform.position + cam.forward);
+        transform.LookAt(transform.position);
     }
 }
