e088e820fa465f3920bc051d14c7226dd4ab32e5
Niko Koivu <niko.koivu@gmail.com>
2020-04-28 19:45:11 +0300

    wip powerup

---
+++ Assets/PowerUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public GameObject pickupEffect;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup(other);
+        }
+    }
+
+    void Pickup(Collider player)
+    {
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+        Debug.Log("powerup picked");
+        StructureScript stats = player.GetComponent<StructureScript>();
+        stats.currentHealth = stats.currentHealth + 100;
+        Destroy(gameObject);
+    }
+}
